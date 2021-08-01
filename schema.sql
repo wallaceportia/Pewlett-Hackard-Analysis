413f025ebdc5190f08f6bcbfaea816a9f53cd383
Portia Wallace <wallaceportia@gmail.com>
2021-07-31 20:55:09 -0500

    retirement table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,5 @@
 -- Creating tables for PH-EmployeeDB
-CREATE TABLE departments (
+CREATE TABLE IF NOT EXISTS departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
      PRIMARY KEY (dept_no),
@@ -8,7 +8,7 @@ CREATE TABLE departments (
 
 SELECT * FROM departments;
 
-CREATE TABLE employees (
+CREATE TABLE IF NOT EXISTS employees (
 	emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
      first_name VARCHAR NOT NULL,
@@ -18,7 +18,9 @@ CREATE TABLE employees (
      PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_manager (
+SELECT * FROM employees;
+
+CREATE TABLE IF NOT EXISTS dept_manager (
 dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
@@ -28,25 +30,25 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
-CREATE TABLE salaries (
+CREATE TABLE IF NOT EXISTS salaries (
   emp_no INT NOT NULL,
   salary INT NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
-CREATE TABLE titles (
+SELECT * FROM salaries;
+
+CREATE TABLE IF NOT EXISTS titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date	DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
-CREATE TABLE dept_emp (
+CREATE TABLE IF NOT EXISTS dept_emp (
 	emp_no INT NOT NULL,
 	dept_no VARCHAR (4) NOT NULL,
 	from_date DATE NOT NULL,
@@ -55,3 +57,8 @@ CREATE TABLE dept_emp (
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
 );
+
+SELECT * FROM dept_emp;
+
+
+
