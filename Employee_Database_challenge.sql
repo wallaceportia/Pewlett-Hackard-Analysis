073671566efa60f4cc627ceaa1a689f2a45d9a90
Portia Wallace <wallaceportia@gmail.com>
2021-08-01 22:37:44 -0500

    mentorship eligibilty csv

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -13,3 +13,22 @@ INTO retiring_titles
 FROM unique_titleS
 GROUP BY title
 ORDER BY count DESC;
+
+--Create a Mentorship Eligibility table
+SELECT DISTINCT ON (e.emp_no) 
+e.emp_no, 
+e.first_name, 
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+titles.title
+INTO mentorship_eligibilty
+FROM employees as e
+INNER JOIN dept_emp as de 
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles
+ON (e.emp_no = titles.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no DESC;
