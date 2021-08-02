3a8b1ea5e2743f3ef9d084ddc4b64135e03a958f
Portia Wallace <wallaceportia@gmail.com>
2021-08-01 21:30:14 -0500

    correction on file and retiring titles

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,7 +1,15 @@
 SELECT DISTINCT ON (employees.emp_no) employees.emp_no, employees.first_name, employees.last_name,
 titles.title,titles.from_date,titles.to_date
-INTO retirement_titles
+INTO unique_titles
 FROM employees
 LEFT JOIN titles on employees.emp_no = titles.emp_no
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY employees.emp_no, titles.to_date DESC;
+
+SELECT * FROM unique_titles;
+
+SELECT COUNT(*) as count, title
+INTO retiring_titles
+FROM unique_titleS
+GROUP BY title
+ORDER BY count DESC;
